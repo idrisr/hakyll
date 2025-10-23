1df57740c9700510ce0d4d2750d0855f25be5d2b
Idris Raja <idris.raja@gmail.com>
2025-10-22 21:08:54 -0500

    fix flake

--- flake.nix
+++ flake.nix
@@ -7,11 +7,33 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         doc = pkgs.callPackage ./doc { };
+        compiler = "ghc984";
+        mysite =
+          pkgs.haskell.packages.${compiler}.callCabal2nix "" ./code/mysite { };
       in
       {
         packages = {
-          default = doc;
           inherit doc;
+          site = pkgs.stdenvNoCC.mkDerivation {
+            pname = "mysite-site";
+            version = "0.1.0.0";
+            src = ./code/mysite;
+            nativeBuildInputs = [ mysite pkgs.coreutils ];
+            LANG = "C.UTF-8";
+            LC_ALL = "C.UTF-8";
+            buildPhase = ''
+              cp -r $src .
+              runHook preBuild
+              ${mysite}/bin/site build
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out"
+              cp -r _site/. "$out/"
+              runHook postInstall
+            '';
+          };
         };
       });
 }
